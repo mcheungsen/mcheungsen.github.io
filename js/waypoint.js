9300be9b33d01f0eb07b8469fe96b9bc733527dc
mcheungsen <mcheungsen@LAPTOP-521O8G61.localdomain>
2021-09-12 16:33:44 +0200

    v2 foreground

---
+++ js/waypoint.js
@@ -0,0 +1,12 @@
+
+let $sections = $('section');
+
+for (var i = 0;  i<$sections.length ; i++){
+    new Waypoint({
+        element: $sections[i],
+        handler: function(direction){
+          $(this.element).toggleClass('js-section-animate');  
+        },
+        offset: '65%',  
+      });
+}
