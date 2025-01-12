2350abcb9a5fa5a889f23723ca69f39c967721e0
mcheungsen <maevachgsn@gmail.com>
2025-01-12 17:18:22 +0100

    :wrench: add config github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
+  app: {
+    baseURL: '/mcheungsen.github.io/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  }
 })
