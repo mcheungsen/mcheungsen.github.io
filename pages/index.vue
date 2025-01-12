e989957e13b3f182b395b0655147e513ae313f1c
mcheungsen <maevachgsn@gmail.com>
2025-01-12 17:14:33 +0100

    :construction: add configs to test deployment

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="h-screen flex items-center justify-center flex-col space-y-8">
+        <div class="text-center font-light">
+            <h1 class="font-serif text-4xl">Maeva Cheung-Sen</h1>
+            <h2 class="text-gray-700 tracking-wide text-xl">Apodis Phoenix</h2>
+        </div>
+        <ul class="space-y-5 border p-10">
+            <li><UIcon name="material-symbols:settings-outline" class="w-5 h-5" /> Configuration de la page web     <UIcon name="fluent-color:checkmark-circle-16" class="w-5 h-5" /></li>
+            <li><UIcon name="simple-icons:nuxtdotjs" class="w-5 h-5" /> Ajout du Framework Nuxt     <UIcon name="fluent-color:checkmark-circle-16" class="w-5 h-5" /></li>
+            <li><UIcon name="bxl:tailwind-css" class="w-5 h-5" /> Tailwind et Nuxt UI     <UIcon name="fluent-color:checkmark-circle-16" class="w-5 h-5" /></li>
+            <li><UIcon name="material-symbols-light:deployed-code-update-outline-rounded" class="w-5 h-5" /> Déploiement sur Github Pages... <UIcon name="svg-spinners:90-ring" class="w-5 h-5 bg-yellow-500" /></li>
+        </ul>
+    </div>
+
+</template>
